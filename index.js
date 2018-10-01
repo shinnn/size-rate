b5b4682a85e403bbe63d918e054b5ddca60d0ff4
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-10-01 19:42:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint from v4 to v5
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * introduce nyc-config-common to reduce boilerplates

--- index.js
+++ index.js
@@ -9,137 +9,137 @@ const spacer = Symbol('spacer');
 const setWithoutArgumentLengthValidation = Symbol('setWithoutArgumentLengthValidation');
 
 const unsupportedOptions = new Set([
-  'exponent',
-  'fullform',
-  'fullforms',
-  'output',
-  'suffixes'
+	'exponent',
+	'fullform',
+	'fullforms',
+	'output',
+	'suffixes'
 ]);
 
 function validateNumber(num, name) {
-  let error;
+	let error;
 
-  if (typeof num !== 'number') {
-    error = new TypeError(`Expected ${name} to be a non-negative number, but got ${
-      inspectWithKind(num)
-    }.`);
-  } else if (num < 0) {
-    error = new RangeError(`Expected ${name} to be a non-negative number, but got a negative value ${num}.`);
-  } else if (!Number.isFinite(num)) {
-    error = new RangeError(`Expected ${name} to be a non-negative finite number, but got ${num}.`);
-  } else if (num > Number.MAX_SAFE_INTEGER) {
-    error = new RangeError(`Expected ${name} to be a non-negative safe number, but got a too large number.`);
-  }
+	if (typeof num !== 'number') {
+		error = new TypeError(`Expected ${name} to be a non-negative number, but got ${
+			inspectWithKind(num)
+		}.`);
+	} else if (num < 0) {
+		error = new RangeError(`Expected ${name} to be a non-negative number, but got a negative value ${num}.`);
+	} else if (!Number.isFinite(num)) {
+		error = new RangeError(`Expected ${name} to be a non-negative finite number, but got ${num}.`);
+	} else if (num > Number.MAX_SAFE_INTEGER) {
+		error = new RangeError(`Expected ${name} to be a non-negative safe number, but got a too large number.`);
+	}
 
-  if (!error) {
-    return;
-  }
+	if (!error) {
+		return;
+	}
 
-  Error.captureStackTrace(error, validateNumber);
-  throw error;
+	Error.captureStackTrace(error, validateNumber);
+	throw error;
 }
 
 module.exports = class SizeRate {
-  constructor(...args) {
-    const argLen = args.length;
+	constructor(...args) {
+		const argLen = args.length;
 
-    if (argLen !== 1) {
-      throw new RangeError(`Expected 1 argument (<Object>), but got ${
-        argLen === 0 ? 'no' : argLen
-      } arguments.`);
-    }
+		if (argLen !== 1) {
+			throw new RangeError(`Expected 1 argument (<Object>), but got ${
+				argLen === 0 ? 'no' : argLen
+			} arguments.`);
+		}
 
-    if (!args[0] || typeof args[0] !== 'object' || Array.isArray(args[0])) {
-      throw new TypeError(`Expected an object to specify SizeRate options, but got ${
-        inspectWithKind(args[0])
-      }.`);
-    }
+		if (!args[0] || typeof args[0] !== 'object' || Array.isArray(args[0])) {
+			throw new TypeError(`Expected an object to specify SizeRate options, but got ${
+				inspectWithKind(args[0])
+			}.`);
+		}
 
-    for (const unsupportedOption of unsupportedOptions) {
-      const val = args[0][unsupportedOption];
+		for (const unsupportedOption of unsupportedOptions) {
+			const val = args[0][unsupportedOption];
 
-      if (val !== undefined) {
-        throw new Error(`\`${unsupportedOption}\` option is not supported, but ${
-          inspectWithKind(val)
-        } was provided.`);
-      }
-    }
+			if (val !== undefined) {
+				throw new Error(`\`${unsupportedOption}\` option is not supported, but ${
+					inspectWithKind(val)
+				} was provided.`);
+			}
+		}
 
-    const options = Object.assign({
-      base: 10,
-      round: 2,
-      spacer: ' ',
-      standard: 'iec'
-    }, args[0], {output: 'array'});
+		const options = Object.assign({ // eslint-disable-line prefer-object-spread
+			base: 10,
+			round: 2,
+			spacer: ' ',
+			standard: 'iec'
+		}, args[0], {output: 'array'});
 
-    validateNumber(options.max, '`max` option');
+		validateNumber(options.max, '`max` option');
 
-    Object.defineProperty(this, round, {value: options.round});
-    Object.defineProperty(this, spacer, {value: options.spacer});
+		Object.defineProperty(this, round, {value: options.round});
+		Object.defineProperty(this, spacer, {value: options.spacer});
 
-    const [value, symbol] = filesize(options.max, options);
-    this.denominator = `${value.toFixed(options.round)}${options.spacer}${symbol}`;
+		const [value, symbol] = filesize(options.max, options);
+		this.denominator = `${value.toFixed(options.round)}${options.spacer}${symbol}`;
 
-    Object.defineProperty(this, formatter, {
-      value: filesize.partial(Object.assign(options, {
-        exponent: filesize(options.max, Object.assign({}, options, {output: 'exponent'}))
-      }))
-    });
+		Object.defineProperty(this, formatter, {
+			value: filesize.partial(Object.assign(options, {
+				exponent: filesize(options.max, Object.assign({}, options, {output: 'exponent'})) // eslint-disable-line prefer-object-spread
+			}))
+		});
 
-    Object.defineProperty(this, 'max', {
-      enumerable: true,
-      value: options.max
-    });
+		Object.defineProperty(this, 'max', {
+			enumerable: true,
+			value: options.max
+		});
 
-    this.bytes = 0;
-  }
+		this.bytes = 0;
+	}
 
-  set(...args) {
-    const argLen = args.length;
+	set(...args) {
+		const argLen = args.length;
 
-    if (argLen !== 1) {
-      throw new RangeError(`Expected 1 argument (<number>), but got ${
-        argLen === 0 ? 'no' : argLen
-      } arguments.`);
-    }
+		if (argLen !== 1) {
+			throw new RangeError(`Expected 1 argument (<number>), but got ${
+				argLen === 0 ? 'no' : argLen
+			} arguments.`);
+		}
 
-    const [num] = args;
+		const [num] = args;
 
-    validateNumber(num, 'an argument of `set` method');
-    this[setWithoutArgumentLengthValidation](num);
-  }
+		validateNumber(num, 'an argument of `set` method');
+		this[setWithoutArgumentLengthValidation](num);
+	}
 
-  format(...args) {
-    const argLen = args.length;
+	format(...args) {
+		const argLen = args.length;
 
-    if (argLen > 1) {
-      throw new RangeError(`Expected 0 or 1 argument (<number>), but got ${argLen} arguments.`);
-    }
+		if (argLen > 1) {
+			throw new RangeError(`Expected 0 or 1 argument (<number>), but got ${argLen} arguments.`);
+		}
 
-    if (argLen === 1) {
-      validateNumber(args[0], 'an argument of `format` method');
-      this[setWithoutArgumentLengthValidation](args[0]);
-    }
+		if (argLen === 1) {
+			validateNumber(args[0], 'an argument of `format` method');
+			this[setWithoutArgumentLengthValidation](args[0]);
+		}
 
-    const result = this[formatter](this.bytes);
-    const str = `${result[0].toFixed(this[round])}${this[spacer]}${result[1]}`;
-    return `${' '.repeat(this.denominator.length - str.length)}${str} / ${this.denominator}`;
-  }
+		const result = this[formatter](this.bytes);
+		const str = `${result[0].toFixed(this[round])}${this[spacer]}${result[1]}`;
+		return `${' '.repeat(this.denominator.length - str.length)}${str} / ${this.denominator}`;
+	}
 };
 
 function internalSet(num) {
-  if (num > this.max) {
-    const error = new RangeError(`Expected a number no larger than the max bytes (${
-      this.max
-    }), but got ${num}.`);
+	if (num > this.max) {
+		const error = new RangeError(`Expected a number no larger than the max bytes (${
+			this.max
+		}), but got ${num}.`);
 
-    Error.captureStackTrace(error, internalSet);
-    throw error;
-  }
+		Error.captureStackTrace(error, internalSet);
+		throw error;
+	}
 
-  this.bytes = num;
+	this.bytes = num;
 }
 
 Object.defineProperty(module.exports.prototype, setWithoutArgumentLengthValidation, {
-  value: internalSet
+	value: internalSet
 });
--- test.js
+++ test.js
@@ -4,117 +4,117 @@ const SizeRate = require('.');
 const test = require('tape');
 
 test('SizeRate', t => {
-  const sizeRate = new SizeRate({max: 123456});
+	const sizeRate = new SizeRate({max: 123456});
 
-  t.equal(
-    sizeRate.format(),
-    '  0.00 KB / 123.46 kB',
-    'should output size rate via `format` method.'
-  );
+	t.equal(
+		sizeRate.format(),
+		'  0.00 KB / 123.46 kB',
+		'should output size rate via `format` method.'
+	);
 
-  sizeRate.set(214.5);
+	sizeRate.set(214.5);
 
-  t.equal(
-    sizeRate.format(),
-    '  0.21 kB / 123.46 kB',
-    'should update a numerator via `set` method.'
-  );
+	t.equal(
+		sizeRate.format(),
+		'  0.21 kB / 123.46 kB',
+		'should update a numerator via `set` method.'
+	);
 
-  t.equal(
-    sizeRate.format(99999),
-    '100.00 kB / 123.46 kB',
-    'should accept an argument of `format` method.'
-  );
+	t.equal(
+		sizeRate.format(99999),
+		'100.00 kB / 123.46 kB',
+		'should accept an argument of `format` method.'
+	);
 
-  t.equal(
-    new SizeRate({max: 0, round: 0}).format(0),
-    '0 B / 0 B',
-    'should support zero.'
-  );
+	t.equal(
+		new SizeRate({max: 0, round: 0}).format(0),
+		'0 B / 0 B',
+		'should support zero.'
+	);
 
-  t.equal(
-    new SizeRate({max: 219021120, base: 2, round: 3}).format(191897743),
-    '183.008 MiB / 208.875 MiB',
-    'should support options for `filesize` module.'
-  );
+	t.equal(
+		new SizeRate({max: 219021120, base: 2, round: 3}).format(191897743),
+		'183.008 MiB / 208.875 MiB',
+		'should support options for `filesize` module.'
+	);
 
-  t.throws(
-    () => new SizeRate([]),
-    /TypeError.*Expected an object to specify SizeRate options, but got \[\] \(array\)\./u,
-    'should throw an error when the constructor receives an invalid value.'
-  );
+	t.throws(
+		() => new SizeRate([]),
+		/TypeError.*Expected an object to specify SizeRate options, but got \[\] \(array\)\./u,
+		'should throw an error when the constructor receives an invalid value.'
+	);
 
-  t.throws(
-    () => new SizeRate({}),
-    /TypeError.*Expected `max` option to be a non-negative number, but got undefined\./u,
-    'should throw an error when the constructor doesn\'t receive `max` option.'
-  );
+	t.throws(
+		() => new SizeRate({}),
+		/TypeError.*Expected `max` option to be a non-negative number, but got undefined\./u,
+		'should throw an error when the constructor doesn\'t receive `max` option.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: new Int32Array()}),
-    /TypeError.*Expected `max` option to be a non-negative number, but got Int32Array \[\]\./u,
-    'should throw an error when `max` option is not a number.'
-  );
+	t.throws(
+		() => new SizeRate({max: new Int32Array()}),
+		/TypeError.*Expected `max` option to be a non-negative number, but got Int32Array \[\]\./u,
+		'should throw an error when `max` option is not a number.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: -1}),
-    /RangeError.*Expected `max` option to be a non-negative number, but got a negative value -1\./u,
-    'should throw an error when `max` option is negative.'
-  );
+	t.throws(
+		() => new SizeRate({max: -1}),
+		/RangeError.*Expected `max` option to be a non-negative number, but got a negative value -1\./u,
+		'should throw an error when `max` option is negative.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: Infinity}),
-    /RangeError.*Expected `max` option to be a non-negative finite number, but got Infinity\./u,
-    'should throw an error when `max` option is infinite.'
-  );
+	t.throws(
+		() => new SizeRate({max: Infinity}),
+		/RangeError.*Expected `max` option to be a non-negative finite number, but got Infinity\./u,
+		'should throw an error when `max` option is infinite.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: Number.MAX_SAFE_INTEGER + 1}),
-    /RangeError.*Expected `max` option to be a non-negative safe number, but got a too large number\./u,
-    'should throw an error when `max` option is larger than the max safe integer.'
-  );
+	t.throws(
+		() => new SizeRate({max: Number.MAX_SAFE_INTEGER + 1}),
+		/RangeError.*Expected `max` option to be a non-negative safe number, but got a too large number\./u,
+		'should throw an error when `max` option is larger than the max safe integer.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: 0.1, fullform: true}),
-    /Error.*`fullform` option is not supported, but true \(boolean\) was provided\./u,
-    'should throw an error when it takes unsupported options.'
-  );
+	t.throws(
+		() => new SizeRate({max: 0.1, fullform: true}),
+		/Error.*`fullform` option is not supported, but true \(boolean\) was provided\./u,
+		'should throw an error when it takes unsupported options.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: 123.45}).set(123.46),
-    /RangeError.*Expected a number no larger than the max bytes \(123\.45\), but got 123\.46\./u,
-    'should throw an error when `set` method receives a number larger than the max value.'
-  );
+	t.throws(
+		() => new SizeRate({max: 123.45}).set(123.46),
+		/RangeError.*Expected a number no larger than the max bytes \(123\.45\), but got 123\.46\./u,
+		'should throw an error when `set` method receives a number larger than the max value.'
+	);
 
-  t.throws(
-    () => new SizeRate(),
-    /RangeError.*Expected 1 argument \(<Object>\), but got no arguments\./u,
-    'should throw an error when the constructor receives no argument.'
-  );
+	t.throws(
+		() => new SizeRate(),
+		/RangeError.*Expected 1 argument \(<Object>\), but got no arguments\./u,
+		'should throw an error when the constructor receives no argument.'
+	);
 
-  t.throws(
-    () => new SizeRate({max: 1}, {max: 2}),
-    /RangeError.*Expected 1 argument \(<Object>\), but got 2 arguments\./u,
-    'should throw an error when the constructor receives too many arguments.'
-  );
+	t.throws(
+		() => new SizeRate({max: 1}, {max: 2}),
+		/RangeError.*Expected 1 argument \(<Object>\), but got 2 arguments\./u,
+		'should throw an error when the constructor receives too many arguments.'
+	);
 
-  t.throws(
-    () => sizeRate.set(),
-    /RangeError.*Expected 1 argument \(<number>\), but got no arguments\./u,
-    'should throw an error when `set` method receives no argument.'
-  );
+	t.throws(
+		() => sizeRate.set(),
+		/RangeError.*Expected 1 argument \(<number>\), but got no arguments\./u,
+		'should throw an error when `set` method receives no argument.'
+	);
 
-  t.throws(
-    () => sizeRate.set(1, 2),
-    /RangeError.*Expected 1 argument \(<number>\), but got 2 arguments\./u,
-    'should throw an error when `set` method receives too many arguments.'
-  );
+	t.throws(
+		() => sizeRate.set(1, 2),
+		/RangeError.*Expected 1 argument \(<number>\), but got 2 arguments\./u,
+		'should throw an error when `set` method receives too many arguments.'
+	);
 
-  t.throws(
-    () => sizeRate.format(1, 2),
-    /RangeError.*Expected 0 or 1 argument \(<number>\), but got 2 arguments\./u,
-    'should throw an error when `format` method receives too many arguments.'
-  );
+	t.throws(
+		() => sizeRate.format(1, 2),
+		/RangeError.*Expected 0 or 1 argument \(<number>\), but got 2 arguments\./u,
+		'should throw an error when `format` method receives too many arguments.'
+	);
 
-  t.end();
+	t.end();
 });
